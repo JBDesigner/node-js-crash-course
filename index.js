dbcdc93863b2541a34b0b0de45e6d609628f898c
Jimmy <jimmybrand95@gmail.com>
2025-11-11 16:35:03 +0100

    Implement dynamic file serving and add 404 error page

--- index.js
+++ index.js
@@ -42,7 +42,54 @@ const server = http.createServer((req, res) => {
         req.url === '/' ? 'index.html' : req.url
     );
 
-    console.log(filePath);
+    // console.log(filePath);
+    // res.end();
+
+    //extension of file
+    let extname = path.extname(filePath);
+
+    //initial content type
+    let contentType = 'text/html';
+
+    //check ext and set content type
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;
+    }
+
+    //read file
+    fs.readFile ( filePath, (err, content) => {
+        if (err) {
+            if (err.code == 'ENOENT') {
+                //page not found
+                fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) => {
+                    res.writeHead(200, { 'Content-Type': 'text/html' });
+                    res.end(content, 'utf8');
+                });
+            } else {
+                //some server error
+                res.writeHead(500);
+                res.end(`Server Error: ${err.code}`);
+            }
+        } else {
+            //success
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(content, 'utf8');
+        }
+    });
 });
 
 const PORT = process.env.PORT || 5000;
